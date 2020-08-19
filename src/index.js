96474820243942fd225f1ef7276db056c6246ad9
Javier Guerra <javiguerra144@gmail.com>
2020-08-19 11:29:08 +0200

    Add Semantic UI

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import 'semantic-ui-css/semantic.min.css';
 
 import Register from './components/Auth/Register';
 import Login from './components/Auth/Login';
