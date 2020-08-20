f708bf36354c1d0b35b4f65d327187938b0ca8ee
Javier Guerra <javiguerra144@gmail.com>
2020-08-20 12:06:30 +0200

    Add sidepanel and sign out functionality

--- src/index.js
+++ src/index.js
@@ -1,37 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import * as serviceWorker from './serviceWorker';
-import firebase from './firebase';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import * as serviceWorker from "./serviceWorker";
+import firebase from "./firebase";
 
-import { BrowserRouter as Router, Switch, Route, withRouter } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  withRouter,
+} from "react-router-dom";
 
-import { createStore } from 'redux';
-import { Provider, connect } from 'react-redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { createStore } from "redux";
+import { Provider, connect } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-import 'semantic-ui-css/semantic.min.css';
+import "semantic-ui-css/semantic.min.css";
 
-import Register from './components/Auth/Register';
-import Login from './components/Auth/Login';
-import rootReducer from './reducers';
-import { setUser } from './actions';
-import Spinner from './Spinner';
+import Register from "./components/Auth/Register";
+import Login from "./components/Auth/Login";
+import rootReducer from "./reducers";
+import { setUser, clearUser } from "./actions";
+import Spinner from "./Spinner";
 
 const store = createStore(rootReducer, composeWithDevTools());
 
 class Root extends React.Component {
   componentDidMount() {
-    firebase.auth().onAuthStateChanged(user => {
+    firebase.auth().onAuthStateChanged((user) => {
       if (user) {
         this.props.setUser(user);
-        this.props.history.push('/');
+        this.props.history.push("/");
+      } else {
+        this.props.history.push("/login");
+        this.props.clearUser();
       }
     });
   }
 
   render() {
-    return this.props.isLoading ? <Spinner /> : (
+    return this.props.isLoading ? (
+      <Spinner />
+    ) : (
       <Switch>
         <Route exact path="/" component={App} />
         <Route path="/login" component={Login} />
@@ -41,11 +51,13 @@ class Root extends React.Component {
   }
 }
 
-const mapStateFromProps = state => ({
-  isLoading: state.user.isLoading
+const mapStateFromProps = (state) => ({
+  isLoading: state.user.isLoading,
 });
 
-const RootWithAuth = withRouter(connect(mapStateFromProps, { setUser })(Root));
+const RootWithAuth = withRouter(
+  connect(mapStateFromProps, { setUser, clearUser })(Root)
+);
 
 ReactDOM.render(
   <Provider store={store}>
@@ -53,7 +65,7 @@ ReactDOM.render(
       <RootWithAuth />
     </Router>
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
