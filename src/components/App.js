705beb337632f42da6eaf8f641ddd76690bfaf51
Javier Guerra <javiguerra144@gmail.com>
2020-08-20 18:49:54 +0200

    Add messages component

--- src/components/App.js
+++ src/components/App.js
@@ -10,13 +10,17 @@ import MetaPanel from "./MetaPanel/MetaPanel";
 
 import "./App.css";
 
-const App = ({ currentUser }) => (
+const App = ({ currentUser, currentChannel }) => (
   <Grid columns="equal" className="app" style={{ background: "#eee" }}>
     <ColorPanel />
-    <SidePanel currentUser={currentUser} />
+    <SidePanel key={currentUser && currentUser.id} currentUser={currentUser} />
 
     <Grid.Column style={{ marginLeft: 320 }}>
-      <Messages />
+      <Messages
+        key={currentChannel && currentChannel.id}
+        currentChannel={currentChannel}
+        currentUser={currentUser}
+      />
     </Grid.Column>
 
     <Grid.Column width={4}>
@@ -27,6 +31,7 @@ const App = ({ currentUser }) => (
 
 const mapStateToProps = (state) => ({
   currentUser: state.user.currentUser,
+  currentChannel: state.channel.currentChannel,
 });
 
 export default connect(mapStateToProps)(App);
