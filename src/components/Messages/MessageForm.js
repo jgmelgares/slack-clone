93957e63b622c95e981d18d510195235d043ef26
Javier Guerra <javiguerra144@gmail.com>
2020-09-03 19:08:01 +0200

    Create shortcut combo keys to send messages

--- src/components/Messages/MessageForm.js
+++ src/components/Messages/MessageForm.js
@@ -29,7 +29,13 @@ class MessageForm extends React.Component {
     this.setState({ [event.target.name]: event.target.value });
   };
 
-  handleKeyDown = () => {
+  handleKeyDown = (event) => {
+    console.log(event.ctrlKey);
+    console.log(event.keyCode);
+    if (event.ctrlKey && event.keyCode === 13) {
+      this.sendMessage();
+    }
+
     const { message, typingRef, channel, user } = this.state;
 
     if (message) {
